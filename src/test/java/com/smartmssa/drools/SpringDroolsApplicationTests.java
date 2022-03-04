87330fa200e7e695c04788a7ce9d3f2e8172c830
medmahmoud <med.kedda@smart-m-solutions.com>
2022-03-04 12:51:17 +0000

    Initial commit

---
+++ src/test/java/com/smartmssa/drools/SpringDroolsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smartmssa.drools;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDroolsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
